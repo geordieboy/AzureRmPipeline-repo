0fc3890d097bdea4499e54692b3e95f2c9791ad5
Lee Atkinson <lee@ladv.co.uk>
2021-12-11 09:45:09 +0000

    fix error

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,3 @@
 # New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/quickstarts/microsoft.logic/logic-app-create/azuredeploy.json
 
-New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/quickstarts/microsoft.logic/logic-app-create/azuredeploy.json
+New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri https://raw.githubusercontent.com/geordieboy/AzureRmPipeline-repo/main/logicAppTemplate.json
