2ba16dfc7c33272fcae485c021f665dc59ffd671
Lee Atkinson <lee@ladv.co.uk>
2021-12-11 19:03:48 +0000

    update template

--- deploy.ps1
+++ deploy.ps1
@@ -1,3 +1,8 @@
-# New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/quickstarts/microsoft.logic/logic-app-create/azuredeploy.json
+New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri https://raw.githubusercontent.com/geordieboy/AzureRmPipeline-repo/main/logicAppTemplate2.json
 
-New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri https://raw.githubusercontent.com/geordieboy/AzureRmPipeline-repo/main/logicAppTemplate.json
+
+#New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri C:\Users\leero\Desktop\azure\AzureRmPipeline-repo\logicAppTemplate.json
+
+#
+
+# New-AzResourceGroupDeployment -ResourceGroupName LADV -TemplateUri https://raw.githubusercontent.com/Azure/azure-quickstart-templates/master/quickstarts/microsoft.logic/logic-app-create/azuredeploy.json
